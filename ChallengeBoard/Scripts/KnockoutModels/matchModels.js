cbcb56b891fd69691d636649c85af3f788b838c8
jmitchell <jmitchell@verascape.com>
2015-02-26 20:25:33 -0600

    Match withdrawls. (interface side)

--- ChallengeBoard/Scripts/KnockoutModels/matchModels.js
+++ ChallengeBoard/Scripts/KnockoutModels/matchModels.js
@@ -13,4 +13,22 @@ function MatchModel() {
     self.tied = ko.observable();
     self.winnerComment = ko.observable();
     self.loserComment = ko.observable();
+    self.action = ko.observable("");
+    self.confirmation = ko.observable("test");
+
+    self.actionDescription = ko.computed(function() {
+        if (self.action().toLowerCase() === "withdraw")
+            return "Type \"withdraw\" to remove this match.";
+        else
+            return "Type \"verify\" or \"reject\" to finalize this match.";
+    });
+
+    self.actionConfirmed = ko.computed(function () {
+        
+        if (self.action().toLowerCase() === "withdraw") {
+            return self.confirmation().toLowerCase() === "withdraw";
+        }
+
+        return (self.confirmation().toLowerCase() === "reject" || self.confirmation().toLowerCase() === "verify");
+    });
 };
